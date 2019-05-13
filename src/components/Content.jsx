75fcec1c4541e2aba6677f9e714f5d4509325212
Rubén Villar Grela <ruben@rubenvillar.net>
2019-05-13 21:30:26 +0200

    trying to add react-admin: FAIL

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,18 +5,13 @@ import { Redirect } from 'react-router-dom'
 
 
 
+
 class Content extends Component {
   render() {
     if(!this.props.user) return <Redirect to='/' />
     return (
       <div>
-        <h1>Todos list</h1>
-        <p>Total todos: {this.props.todos}</p>
-        <button
-        onClick={()=> this.props.increment()}>+</button>
-        <br/>
-        <button
-        onClick={()=> this.props.decrement()}>-</button>
+     
       </div>
     )
   }
