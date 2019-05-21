81b64a627a07090461fc012f186725f8b9b50a32
Rubén Villar Grela <ruben@rubenvillar.net>
2019-05-21 16:23:47 +0200

    Updates

---
+++ precache-manifest.c2ab68bce0c7f4ab1d47ec6d7bf12fb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e02afbcd93aec0ddd7feb3aa5815dfc",
+    "url": "/CD-React-test/index.html"
+  },
+  {
+    "revision": "e4cf1c5b61765c00c1d4",
+    "url": "/CD-React-test/static/css/main.3a25237a.chunk.css"
+  },
+  {
+    "revision": "3966441881e1e488a9aa",
+    "url": "/CD-React-test/static/js/2.4635a162.chunk.js"
+  },
+  {
+    "revision": "e4cf1c5b61765c00c1d4",
+    "url": "/CD-React-test/static/js/main.3d128b26.chunk.js"
+  },
+  {
+    "revision": "b289e0a2722f004f0bd5",
+    "url": "/CD-React-test/static/js/runtime~main.a6f91ad2.js"
+  }
+]);
